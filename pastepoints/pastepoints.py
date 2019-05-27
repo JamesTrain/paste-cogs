456db0eee1f08ed68b407b64bea6bb20cdfd9f11
JamesTrain <thejamestrain@gmail.com>
2019-05-26 23:03:54 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -14,8 +14,15 @@ class PastePoints(BaseCog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group(autohelp=False)
+    @commands.guild_only()
+    @checks.admin_or_permissions(manage_guild=True)
     async def points(self, ctx):
-        """This does stuff!"""
+        """Paste Points Are Cool"""
         # Your code will go here
         await ctx.send("I can do stuff!")
+
+    @points.command
+    async def _points_on(self, ctx):
+        """This is a huge meme"""
+        await ctx.send("This is the biggest meme")
