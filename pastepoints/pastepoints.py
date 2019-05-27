198cfe6221fe5577cdf8ebc57737433b028a3d2e
JamesTrain <thejamestrain@gmail.com>
2019-05-26 22:41:58 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -6,13 +6,13 @@ from redbot.core import Config, checks, commands
 
 # Paste Points are the new Bitcoin
 class PastePoints(commands.Cog):
-    """My custom cog"""
+    """Paste Points cog settings"""
 
     @commands.group(autohelp=False)
     @commands.guild_only()
     @checks.admin_or_permissions(manage_guild=True)
-    async def points(self, ctx, message):
-        """This does stuff!"""
-        # Your code will go here
+    async def points(self, ctx):
         await ctx.send("This works")
-        await message.add_reaction(emoji="redCross:423541694600970243")
+
+    async def on_message(self, ctx):
+        await ctx.send("memes")
