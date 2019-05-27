ccc78725178d65ec6645d6228d27e7402ecede2a
JamesTrain <thejamestrain@gmail.com>
2019-05-27 18:03:19 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -111,7 +111,7 @@ class PastePoints(BaseCog):
         if author == user or isinstance(channel, discord.abc.PrivateChannel): #fix this
             return
         #emoji = self.fix_custom_emoji(reaction.emoji)
-        print (reaction.emoji.id)
+        print (reaction.emoji)
         if (reaction.emoji.id == upemoji_id):
             print ('DEBUG: This is an upvote')
             await self._add_karma(author, 1 if added == True else -1)
