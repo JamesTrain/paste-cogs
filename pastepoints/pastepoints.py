ce681d431860082ec7e1a175e777677f49baec95
JamesTrain <thejamestrain@gmail.com>
2019-05-27 03:23:38 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -36,5 +36,7 @@ class PastePoints(BaseCog):
         if (message.channel.id == 331655111644545027):
             await message.add_reaction('\u2705')
             await message.add_reaction('\u274C')
-        await message.add_reaction(emoji='<:bolbmoji:397064398830829569>')
-        await message.add_reaction('<:FeelsBadMan:272737368916754432>')
+        upemoji = '<:bolbmoji:397064398830829569>'
+        downemoji = '<:FeelsBadMan:272737368916754432>'
+        await message.add_reaction(upemoji)
+        await message.add_reaction(downemoji)
