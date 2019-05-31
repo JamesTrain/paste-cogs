ada60f5506c448ee30b20a89e969bfed54484414
JamesTrain <thejamestrain@gmail.com>
2019-05-31 18:38:53 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -93,7 +93,7 @@ class PastePoints(BaseCog):
         if (message.author.id == self.bot.user.id):
             return
         if (message.channel.id == channel_id):
-            print (message.type)
+            print (message.attachments)
             upemoji = self.bot.get_emoji(upemoji_id)
             downemoji = self.bot.get_emoji(downemoji_id)
             await message.add_reaction(upemoji)
