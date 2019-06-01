6735838bdf585cdde2f91c281a390bbbc8a2d8f0
JamesTrain <thejamestrain@gmail.com>
2019-06-01 01:26:01 -0400

    asdf

--- events/events.py
+++ events/events.py
@@ -24,5 +24,7 @@ class Events(BaseCog):
     @checks.admin_or_permissions(manage_guild=True)
     async def events(self, ctx):
         """These Are Events"""
-
-        await ctx.send("Yeye")
+        embed = discord.Embed(title="Event Planner", description="How to use the event planner", color=0x00ff00)
+            embed.add_field(name="Field1", value="hi", inline=False)
+            embed.add_field(name="Field2", value="hi2", inline=False)
+            await client.send_message(message.channel, embed=embed)
