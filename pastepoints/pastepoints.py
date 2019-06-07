b2f233f52bfe83fa1b75fe92a75de294215db894
JamesTrain <thejamestrain@gmail.com>
2019-06-06 22:45:53 -0400

    asdf

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -90,8 +90,7 @@ class PastePoints(BaseCog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        print (re.search("http:\/\/|https:\/\/", message.content))
-        if (message.author.id == self.bot.user.id or (message.attachments == [] and message.embeds == [] and re.search("http:\/\/|https:\/\/", message.content) == None):
+        if (message.author.id == self.bot.user.id or (message.attachments == [] and message.embeds == [] and re.search("http:\/\/|https:\/\/", message.content) == None)):
             return
         if (message.channel.id == channel_id):
             upemoji = self.bot.get_emoji(upemoji_id)
