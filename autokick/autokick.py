f3000f1970f363e32668e16e900f05e8945a1400
James Gallagher <thejamestrain@gmail.com>
2019-10-08 23:38:20 -0400

    autokick

--- autokick/autokick.py
+++ autokick/autokick.py
@@ -25,7 +25,7 @@ class AutoKick(BaseCog):
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         a = self.bot.get_channel(364238602957226004)
-        await a.send(after)
+        await a.send(after.voice)
         #if (after.voice.channel == 502108640530923520):
             #await a.send("FUCKKKKKKK")
             #await after.id.move_to(channel=None, reason="No Furries Allowed")
