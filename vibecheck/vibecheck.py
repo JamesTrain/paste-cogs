255cec83b1be392b518c5ae6b4a6b636bead8735
James Gallagher <thejamestrain@gmail.com>
2020-09-02 01:30:28 -0400

    Added vibecheck

--- vibecheck/vibecheck.py
+++ vibecheck/vibecheck.py
@@ -25,8 +25,25 @@ class Vibecheck(BaseCog):
         self.config = Config.get_conf(self, identifier=974374574)
         default_guild = {}
         self.config.register_guild(**default_guild)
+        self.config.register_user(vibe=0)
+        self.config.register_user(hasrun=0)
 
     @commands.command()
     async def vibecheck(self, ctx: commands.Context):
         """Check your vibes"""
-        await ctx.send(":game_die: {} checked their vibe and got {}".format(ctx.message.author.mention,randint(1, 20)))
+        vibe = randint(1, 20)
+        await ctx.send(":game_die: {} checked their vibe and got {}".format(
+            ctx.message.author.mention,vibe)
+        )
+        if vibe == 1:
+            await ctx.send("lmaooo")
+        if vibe < 6:
+            await ctx.send("Sucks for you bro")
+        elif vibe < 11:
+            await ctx.send("I guess that's fine")
+        elif vibe < 16:
+            await ctx.send("Pretty sick dude")
+        elif vibe < 20:
+            await ctx.send("Vibin hard")
+        else:
+            await ctx.send("So much :cum:")
