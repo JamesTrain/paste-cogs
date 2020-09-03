9c2ad1893cff5c7f96c67d44474e8f3929adee00
James Gallagher <thejamestrain@gmail.com>
2020-09-03 00:33:39 -0400

    Adding timezone support

--- vibecheck/vibecheck.py
+++ vibecheck/vibecheck.py
@@ -36,6 +36,7 @@ class Vibecheck(BaseCog):
         """Check your vibes"""
         lastranstr = await self.config.user(ctx.message.author).lastran()
         lastran = datetime.datetime.strptime(lastranstr, "%Y-%m-%d").date()
+        print("Last ran {}. Now {}".format(lastran, datetime.date.now()))
 
         if datetime.date.today() == lastran:
             vibe = await self.config.user(ctx.message.author).vibe()
