735e077eea7ba2678de8a8ed98751278c25014c0
James Gallagher <thejamestrain@gmail.com>
2020-10-10 12:57:49 -0400

    Trying to fix a big in autokick

--- autokick/autokick.py
+++ autokick/autokick.py
@@ -21,7 +21,7 @@ class AutoKick(BaseCog):
         self.config.register_guild(**default_guild)
 
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if (after.channel.channel_id == 502108640530923520):
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if (after.channel == 502108640530923520):
             time.sleep(1)
             await member.move_to(channel=None, reason="Get rekd nerd")
