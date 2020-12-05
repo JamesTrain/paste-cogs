47244a69008adef45c3243d13eb4c2437e48fbf1
James Gallagher <thejamestrain@gmail.com>
2020-12-05 05:13:06 -0500

    changing downvote emoji

--- messagelog/messagelog.py
+++ messagelog/messagelog.py
@@ -14,6 +14,10 @@ from redbot.core.utils.chat_formatting import box, pagify
 
 BaseCog = getattr(commands, "Cog", object)
 
+upemoji_id = 397064398830829569
+downemoji_id = 272737368916754432
+meme_channel_id = 331655111644545027
+
 class MessageLog(BaseCog):
     """Message Log cog settings meme"""
 
@@ -26,6 +30,49 @@ class MessageLog(BaseCog):
     @commands.Cog.listener()
     async def on_message(self, message):
         await self._insert_message(message)
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
+        """Fires when the bot sees a reaction being added, and updates karma."""
+        await self._check_reaction(reaction, user, added=True, Reaction.message)
+
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction: discord.Reaction, user: discord.User, message):
+        """Fires when the bot sees a reaction being removed, and updates karma."""
+        await self._check_reaction(reaction, user, added=False, message)
+
+    async def _check_reaction(self, reaction: discord.Reaction, user: discord.User, *, added: bool, message):
+        message = reaction.message
+        (author, channel, guild) = (message.author, message.channel, message.guild)
+        if author == user:
+            return
+        if (reaction.emoji.id == upemoji_id):
+            await self._add_karma(author, 1 if added == True else -1, message)
+        elif (reaction.emoji.id == downemoji_id):
+            await self._add_karma(author, -1 if added == True else 1, message)
+
+    async def _add_karma(self, user: discord.User, amount: int):
+        cnx = mysql.connector.connect(
+        host="localhost",
+        user="pastebot",
+        password="8Kv7@D5S@HuQ",
+        database="paste"
+        )
+        cursor = cnx.cursor()
+
+        # Get current upvotes
+        cursor.execute("SELECT upvotes FROM messages WHERE message_id = '{}'".format(message.id))
+        upvotes = mycursor.fetchall()
+
+        #Add 1 to the count
+        upvotes += amount
+        cursor.execute("SELECT upvotes FROM messages WHERE message_id = '{}'".format(message.id))
+
+        # Make sure data is committed to the database
+        cnx.commit()
+
+        cursor.close()
+        cnx.close()
 
     async def _insert_message(self, message):
         cnx = mysql.connector.connect(
