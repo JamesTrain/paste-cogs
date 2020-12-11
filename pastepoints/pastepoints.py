8e3c0abd02035a12d5eb30ff17a28e6e02bfccf1
James Gallagher <thejamestrain@gmail.com>
2020-12-11 18:13:57 -0500

    testing

--- pastepoints/pastepoints.py
+++ pastepoints/pastepoints.py
@@ -92,13 +92,13 @@ class PastePoints(BaseCog):
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: discord.User):
         """Fires when the bot sees a reaction being added, and updates karma."""
-        if reaction.emoji.id == upemoji_id or reaction.emoji.id == downemoji_id:
+        if type(reaction.emoji.id) is int:
             await self._check_reaction(reaction, user, added=True)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction: discord.Reaction, user: discord.User):
         """Fires when the bot sees a reaction being removed, and updates karma."""
-        if reaction.emoji.id == upemoji_id or reaction.emoji.id == downemoji_id:
+        if type(reaction.emoji.id) is int:
             await self._check_reaction(reaction, user, added=False)
 
     async def _check_reaction(self, reaction: discord.Reaction, user: discord.User, *, added: bool):
