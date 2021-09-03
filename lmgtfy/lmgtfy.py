e9b0e8f19d015b66b72a46278cd8a3b19110b15b
root <root@localhost.localdomain>
2021-09-03 16:59:39 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -33,7 +33,7 @@ class lmgtfy(commands.Cog):
                 output = "https://lmgtfy.app/?q="
                 for l in o:
                     output = ''.join([output, l+'+'])
-                return discord.Attachment.str(output[:-1]+'?')
+                return discord.Attachment(str(output[:-1]+'?'))
             elif '?' not in i[::-1]:
                 continue
             else:
