0de3e03550a197917a809e947c0840a886fa9228
root <root@localhost.localdomain>
2021-09-03 11:54:28 -0400

    INIT path for lmgtfy.py

---
+++ lmgtfy/__init__.py
@@ -0,0 +1,7 @@
+from .lmgtfy import lmgtfy
+
+
+def setup(bot):
+    bot.add_cog((lmgtfy))
+
+# Author - Daniel Bush, A.K.A. Daddy
---
+++ lmgtfy/lmgtfy.py
@@ -0,0 +1,21 @@
+import discord
+from redbot.core import commands
+from .pcx_lib import type_message
+
+class lmgtfy(commands.Cog):
+    @commands.command(aliases=[""])
+    async def lmgtfy(self, ctx:commands.Context):
+        message = (await ctx.channel.history(limit=2).flatten())[1].content
+        if not message:
+            message = "This isn't a question. You a dumb person."
+        else:
+            await type_message(
+                ctx.channel,
+                self.lmgtfy(ctx, User_Mention),
+                allowed_mentions=discord.AllowedMentions(
+                    everyone=False, users=False,
+                    roles=False
+                ),
+            )
+
+    def lmgtfy (self, ctx, User_Mention):
