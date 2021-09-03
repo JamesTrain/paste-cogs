8910cf908534d8c78911ef2e55a1c585caf70f6d
root <root@localhost.localdomain>
2021-09-03 13:21:47 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -1,22 +1,22 @@
 import discord
 from redbot.core import commands
 from .pcx_lib import type_message
-
+#Redbot cog that takes the above message and converts it to a "lmgtfy" link.
 class lmgtfy(commands.Cog):
-    @commands.command(aliases=[""])
-    async def lmgtfy(self, ctx:commands.Context, User_Mention):
+    @commands.command()
+    async def lmgtfy(self, ctx: commands.Context):
+        #Define the command for redbot
         message = (await ctx.channel.history(limit=2).flatten())[1].content
         if not message:
-            message = "This isn't a question. You a dumb person."
-        else:
-            await type_message(
-                ctx.channel,
-                self.lmgtfy(ctx, User_Mention),
-                allowed_mentions=discord.AllowedMentions(
-                    everyone=False, users=False,
-                    roles=False
-                ),
-            )
-
-    def lmgtfy (self, ctx, User_Mention):
-        return f"This cog loaded properly{User_Mention}"
+            message = "I can't translate that!"
+        await type_message(
+            ctx.channel,
+            self.sarcog_string(message),
+            allowed_mentions=discord.AllowedMentions(
+                everyone=False, users=False, roles=False),
+        )
+    @staticmethod
+    def lmgtfy(self, x):
+        #Convert the above message into lmgtfy link
+        output = []
+        
