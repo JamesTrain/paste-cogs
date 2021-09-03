74b88a504a38150c6d15f272af6f80cd55160079
root <root@localhost.localdomain>
2021-09-03 15:58:05 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -22,7 +22,7 @@ class lmgtfy(commands.Cog):
                 allowed_mentions=discord.AllowedMentions(
                 everyone=False, users=False, roles=False)
                 )
-            except (ValueError, TypeError):
+            except (TypeError):
                 return "I can't seem to find a question. Try again."
 
     @staticmethod
