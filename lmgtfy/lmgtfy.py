41eb7b1d90aa205da8cd6554db19387212e2799c
root <root@localhost.localdomain>
2021-09-03 14:40:23 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -11,12 +11,13 @@ class lmgtfy(commands.Cog):
         message = (await ctx.channel.history(limit=2).flatten())[1].content
         if not message:
             message = "I can't translate that!"
-        await type_message(
-            ctx.channel,
-            self.command(message),
-            allowed_mentions=discord.AllowedMentions(
-                everyone=False, users=False, roles=False),
-        )
+        else:
+            await type_message(
+                ctx.channel,
+                self.command(message),
+                allowed_mentions=discord.AllowedMentions(
+                    everyone=False, users=False, roles=False),
+            )
     @staticmethod
     def command(io):
         #Convert the above message into lmgtfy link
