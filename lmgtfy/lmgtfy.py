4db50f98a8a9d3af731a02a4251b226effb051de
root <root@localhost.localdomain>
2021-09-03 19:40:07 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -9,22 +9,8 @@ from .split import split_into_sentences
 #Redbot cog that takes the above message and converts it to a "lmgtfy" link.
 class lmgtfy(commands.Cog):
     @commands.command()
-    async def lmgtfy(self, ctx: commands.Context):
-        #Define the command for redbot
-        message = (await ctx.channel.history(limit=2).flatten())[1].content
-        if not message:
-            message = "I can't seem to detect any questions."
-        else:
-            await type_message(
-                ctx.channel,
-                self.google(message),
-                allowed_mentions=discord.AllowedMentions(
-                everyone=False, users=False, roles=False)
-                ,
-            )
-
-    @staticmethod
-    def google(io):
+    
+    async def google(io):
         #Convert the above message into lmgtfy link
         sentence = split_into_sentences(io)
         for i in sentence:
@@ -33,7 +19,7 @@ class lmgtfy(commands.Cog):
                 output = "https://lmgtfy.app/?q="
                 for l in o:
                     output = ''.join([output, l+'+'])
-                return '```\n' + output[:-1] + '?' + '\n```'
+                return output[:-1] + '?'
             elif '?' not in i[::-1]:
                 continue
             else:
