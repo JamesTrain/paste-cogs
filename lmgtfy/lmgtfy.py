e0a0da80b389c4fdc4496bcbc09bc5977b18e08c
root <root@localhost.localdomain>
2021-09-03 21:35:09 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -15,14 +15,15 @@ class lmgtfy(commands.Cog):
         message = (await ctx.channel.history(limit=2).flatten())[1].content
         for i in split_into_sentences(message):
             if 'app/?' not in i:
-                if '?' in i[::-1]:
-                    o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
-                    output = "https://lmgtfy.app/?q="
-                    for l in o:
-                        output = ''.join([output, l+'+'])
-                    output = output[:-1]
-                    await ctx.send(output)
-                elif '?' not in i[::-1]:
-                    continue
-                else:
-                    await ctx.send("I can't seem to find a question.")
+                if 'q=' not in i[0:2]:
+                    if '?' in i[::-1]:
+                        o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
+                        output = "https://lmgtfy.app/?q="
+                        for l in o:
+                            output = ''.join([output, l+'+'])
+                        output = output[:-1]
+                        await ctx.send(output)
+                    elif '?' not in i[::-1]:
+                        continue
+                    else:
+                        await ctx.send("I can't seem to find a question.")
