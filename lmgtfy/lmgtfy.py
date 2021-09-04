aa1dbc8a94fcf7ba55f9248f6d15d06917419578
root <root@localhost.localdomain>
2021-09-03 20:05:45 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -40,7 +40,7 @@ class lmgtfy(commands.Cog):
                     description = 'You lazy POS. Google it yourself next time',
                     colour = discord.Colour.red()
                  )
-                embed.add_field(name=output)
+                embed.add_field(name="Here.", value=output)
                 return embed
             elif '?' not in i[::-1]:
                 continue
