f718321e45170877c701765e258cff9cea548bae
root <root@localhost.localdomain>
2021-09-03 21:22:27 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -11,15 +11,18 @@ class lmgtfy(commands.Cog):
     @commands.command()
     async def lmgtfy(self, ctx: commands.Context):
         message = (await ctx.channel.history(limit=2).flatten())[1].content
-        for i in split_into_sentences(message):
-            if '?' in i[::-1]:
-                o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
-                output = "https://lmgtfy.app/?q="
-                for l in o:
-                    output = ''.join([output, l+'+'])
-                output = output[:-1] + '?'
-                await ctx.send(output)
-            elif '?' not in i[::-1]:
-                continue
-            else:
-                await ctx.send("I can't seem to find a question.")
+        try:
+            for i in split_into_sentences(message):
+                if '?' in i[::-1]:
+                    o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
+                    output = "https://lmgtfy.app/?q="
+                    for l in o:
+                        output = ''.join([output, l+'+'])
+                    output = output[:-1] + '?'
+                    await ctx.send(output)
+                elif '?' not in i[::-1]:
+                    continue
+                else:
+                    await ctx.send("I can't seem to find a question.")
+        except (ValueError,TypeError):
+            await ctx.send("I can't make sense of this. Try harder.")
