8157be03d438dadf70897a4ed7ea6549ec49796a
root <root@localhost.localdomain>
2021-09-03 20:22:19 -0400

    updates

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -12,30 +12,27 @@ class lmgtfy(commands.Cog):
     @commands.command(pass_context=True)
     async def lmgtfy(self, ctx: commands.Context):
         message = (await ctx.channel.history(limit=2).flatten())[1].content
-        if not message:
-            message = "I could not find a question here. Idiot."
-        else:
-            await (ctx.channel,self.google(message))
-    
-    @staticmethod
-    def google(io):
-        sentence = split_into_sentences(io)
-        for i in sentence:
-            if '?' in i[::-1]:
-                o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
-                output = "https://lmgtfy.app/?q="
-                for l in o:
-                    output = ''.join([output, l+'+'])
-                output = output[:-1] + '?'
+ 
+        def google(io):
+            sentence = split_into_sentences(io)
+            for i in sentence:
+                if '?' in i[::-1]:
+                    o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
+                    output = "https://lmgtfy.app/?q="
+                    for l in o:
+                        output = ''.join([output, l+'+'])
+                    output = output[:-1] + '?'
 
-                embed = discord.Embed(
-                    title = 'LMGTFY',
-                    description = 'You lazy POS. Google it yourself next time',
-                    colour = discord.Colour.red()
-                 )
-                embed.add_field(name="Here.", value=output)
-                return embed
-            elif '?' not in i[::-1]:
-                continue
-            else:
-                return "I can't seem to find a question."
+                    embed = discord.Embed(
+                        title = 'LMGTFY',
+                        description = 'You lazy POS. Google it yourself next time',
+                        colour = discord.Colour.red()
+                    )
+                    embed.add_field(name="Here.", value=output)
+                    return embed
+                elif '?' not in i[::-1]:
+                    continue
+                else:
+                    return "I can't seem to find a question."
+            
+        google(message)
