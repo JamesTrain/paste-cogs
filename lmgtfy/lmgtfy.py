ba82b2d6dd91d2685d31dc90fae1c96a47422b32
root <root@localhost.localdomain>
2021-09-22 20:12:00 -0400

    message

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -17,26 +17,23 @@ class lmgtfy(commands.Cog):
         """
         Takes the previous message and splits the text into sentences. If there is a question denoted by a '?', than this command will generate a 'lmgtfy' link with much sass. Otherwise nothing happens.
         """
-        try:
-            message = (await ctx.channel.history(limit=2).flatten())[1].content
-            if not message:
-                await ctx.send("I can't seem to find a question!")
-            for i in split_into_sentences(message):
-                if 'app/?' not in i:
-                    if 'q=' not in i[0:2]:
-                        if '?' in i[::-1]:
-                            o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
-                            output = "https://lmgtfy.app/?q="
-                            for l in o:
-                                output = ''.join([output, l+'+'])
-                            output = output[:-1]
-                            await ctx.send(output)
-                        elif '?' not in i[::-1]:
-                            await ctx.send("I can't seem to find a question.")
-                        else:
-                            await ctx.send("I can't seem to find a question.")
-        except:
-            await ctx.send("I can't seem to find a question.")
+        message = (await ctx.channel.history(limit=2).flatten())[1].content
+        if not message:
+            await ctx.send("I can't seem to find a question!")
+        for i in split_into_sentences(message):
+            if 'app/?' not in i:
+                if 'q=' not in i[0:2]:
+                    if '?' in i[::-1]:
+                        o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
+                        output = "https://lmgtfy.app/?q="
+                        for l in o:
+                            output = ''.join([output, l+'+'])
+                        output = output[:-1]
+                        await ctx.send(output)
+                    elif '?' not in i[::-1]:
+                        await ctx.send("I can't seem to find a question.")
+                    else:
+                        await ctx.send("I can't seem to find a question.")
 
     @commands.command()
     async def lmgtfy(self, ctx: commands.Context, question):
