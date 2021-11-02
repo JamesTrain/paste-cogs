abd2b233979b71adb8806f7478a1c0ed2a5432b0
Daniel Bush <dbbush@student.fullsail.edu>
2021-11-02 08:23:07 -0500

    Fuck Joey: v0.4

--- rmjoey/rmjoey.py
+++ rmjoey/rmjoey.py
@@ -1,9 +1,10 @@
 import discord
-from discord.ext import commands
+from redbot.core import commands
 
 class rmjoey(commands.Cog):
     @commands.Cog.listener()
-    async def on_message(message):
+    async def on_message(self, message):
         if message.author.id == 295400354160443403:
             await message.delete()
             await message.channel.send(f"You can't do that, {message.author.mention}")
+
