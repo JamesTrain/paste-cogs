43b037227a1e4dd4b12f0451cfae21ba1182cce9
Dan <bush.daniel96@gmail.com>
2022-04-13 12:48:08 -0500

    Remove .google command and clean up imports. V1.1.0 status update.

--- lmgtfy/lmgtfy.py
+++ lmgtfy/lmgtfy.py
@@ -1,8 +1,7 @@
 import discord
 import re
 from redbot.core import commands
-from .split import split_into_sentences
-#v0.2.0
+#v1.1.0
 class lmgtfy(commands.Cog):
     """
     Redbot cog that takes a message and makes a 'lmgtfy' link out of it.
@@ -10,27 +9,6 @@ class lmgtfy(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def google(self, ctx: commands.Context):
-        """
-        Creates a 'lmgtfy' link from a previous question
-        """
-        message = (await ctx.channel.history(limit=2).flatten())[1].content
-        if not message:
-            await ctx.send("I can't seem to find a question!")
-        for i in split_into_sentences(message):
-            if 'app/?' not in i:
-                if 'q=' not in i[0:2]:
-                    if '?' in i[::-1]:
-                        o = re.split(r'\s|(?<!\d)[\?](?!\d)/gm', i)
-                        output = "https://lmgtfy.app/?q="
-                        for l in o:
-                            output = ''.join([output, l+'+'])
-                        output = output[:-1]
-                        await ctx.send(output)
-            else:
-                await ctx.send("I can't seem to find a question!")
 
     @commands.command()
     async def lmgtfy(self, ctx: commands.Context, question):
