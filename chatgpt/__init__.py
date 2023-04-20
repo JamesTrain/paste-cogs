e484b674fe3d467bfc4a636ed245ffa2310fda94
jamestrain <3924648+JamesTrain@users.noreply.github.com>
2023-04-20 00:20:53 -0400

    AI

---
+++ chatgpt/__init__.py
@@ -0,0 +1,7 @@
+from .chatgpt import chatgpt
+
+
+def setup(bot):
+    bot.add_cog(chatgpt())
+
+# Author - GPT-4
---
+++ chatgpt/chatgpt.py
@@ -0,0 +1,33 @@
+import discord
+import openai
+import os
+from redbot.core import commands
+
+class ChatGPT(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.api_key = os.environ.get("OPENAI_API_KEY")
+        openai.api_key = self.api_key
+
+    @commands.command(name="chatgpt")
+    async def chat_gpt(self, ctx, *, prompt: str):
+        """
+        Sends a prompt to the OpenAI API and receives a response.
+        """
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=150,
+                n=1,
+                stop=None,
+                temperature=0.7,
+            )
+
+            await ctx.send(f"Response from OpenAI API: {response.choices[0].text.strip()}")
+
+        except Exception as e:
+            await ctx.send(f"Error: {str(e)}")
+
+def setup(bot):
+    bot.add_cog(ChatGPT(bot))
