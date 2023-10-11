7c141927e77bc06ea08eb59f875e9d1c9c9405b5
jamestrain <3924648+JamesTrain@users.noreply.github.com>
2023-10-11 15:49:07 -0400

    Updating extremeuwu

--- extremeuwu/__init__.py
+++ extremeuwu/__init__.py
@@ -1,7 +1,6 @@
+# Author - Daniel Bush, A.K.A. Daddy
+ 
 from .extremeuwu import extremeuwu
 
-
 async def setup(bot):
-    await bot.add_cog(extremeuwu())
-
-# Author - Daniel Bush, A.K.A. Daddy
+    await bot.add_cog(extremeuwu(bot))
