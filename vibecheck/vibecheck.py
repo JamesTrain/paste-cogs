be711ed2c437bc742ec4fc55881abb3a89e0a6b3
James Gallagher <jgallagher@moderncampus.com>
2025-03-28 21:16:27 -0400

    refactor(vibecheck): Improve code organization and reliability

--- vibecheck/vibecheck.py
+++ vibecheck/vibecheck.py
@@ -1,69 +1,81 @@
-import asyncio, io, re, datetime, time, os
-from collections import namedtuple
+import datetime
+import time
+import os
+from typing import Dict, Optional
 
-import aiohttp
 import discord
-from redbot.core import Config, checks, commands
-from redbot.core.utils.chat_formatting import box, pagify
-
-# generate random integer values
-from random import seed
-from random import randint
-# seed random number generator
-seed(1)
+from redbot.core import Config, commands
 
 # Set timezone
 os.environ['TZ'] = 'EST'
 time.tzset()
 
-BaseCog = getattr(commands, "Cog", object)
+class Vibecheck(commands.Cog):
+    """Check them vibes
+    
+    A cog that allows users to check their daily vibes.
+    Users can check their vibes once per day, getting a random score
+    between 0 and 20 with corresponding comments.
+    """
 
-class Vibecheck(BaseCog):
-    """Check them vibes"""
+    VIBE_COMMENTS: Dict[int, str] = {
+        0: "Literally die idiot 💀",
+        1: "Uh oh stinky!",
+        2: "Your vibes are catastrophically bad",
+        4: "That's gonna be an oof from me dog",
+        6: "Not looking good fam",
+        8: "You might want to go back to bed",
+        10: "That's almost a vibe",
+        12: "Getting better, keep it up",
+        14: "Alright, now that's a meme",
+        16: "It's gonna be a good day :)",
+        18: "Absolutely vibing!",
+        20: "IMMACULATE VIBES, YOUR MAJESTY 👑"
+    }
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier = 974374574)
+        self.config = Config.get_conf(self, identifier=974374574)
         default_guild = {}
         self.config.register_guild(**default_guild)
-        self.config.register_user(vibe = 0)
+        self.config.register_user(vibe=0)
 
         yesterday = datetime.date.today() - datetime.timedelta(days=1)
-        self.config.register_user(lastran = datetime.datetime.strftime(yesterday, "%Y-%m-%d"))
+        self.config.register_user(lastran=datetime.datetime.strftime(yesterday, "%Y-%m-%d"))
+
+    def _get_vibe_comment(self, vibe: int) -> str:
+        """Get the appropriate comment for a given vibe score."""
+        for threshold in sorted(self.VIBE_COMMENTS.keys(), reverse=True):
+            if vibe >= threshold:
+                return self.VIBE_COMMENTS[threshold]
+        return self.VIBE_COMMENTS[0]
 
     @commands.command()
     async def vibecheck(self, ctx: commands.Context):
-        """Check your vibes"""
-        lastranstr = await self.config.user(ctx.message.author).lastran()
-        lastran = datetime.datetime.strptime(lastranstr, "%Y-%m-%d").date()
-        print("Last ran {}. Now {}".format(lastran, datetime.date.today()))
+        """Check your vibes for the day.
+        
+        Users can only check their vibes once per day.
+        Returns a random score between 0 and 20 with a corresponding comment.
+        """
+        try:
+            lastranstr = await self.config.user(ctx.message.author).lastran()
+            lastran = datetime.datetime.strptime(lastranstr, "%Y-%m-%d").date()
 
-        if datetime.date.today() == lastran:
-            vibe = await self.config.user(ctx.message.author).vibe()
-            await ctx.send("You rolled a **{}** today.".format(vibe))
-        else:
-            await self.config.user(ctx.message.author).lastran.set(
-                datetime.datetime.strftime(datetime.date.today(), "%Y-%m-%d")
-            )
-                
-            vibe = randint(0, 20)    
-            await self.config.user(ctx.message.author).vibe.set(vibe)
-
-            if vibe < 1:
-                comment = "Literally die idiot 💀"
-            elif vibe < 2:
-                comment = "Uh oh stinky!"
-            elif vibe < 6:
-                comment = "That's gonna be an oof from me dog"
-            elif vibe < 11:
-                comment = "That's almost a vibe"
-            elif vibe < 16:
-                comment = "Alright, now that's a meme"
-            elif vibe < 20:
-                comment = "It's gonna be a good day :)"
+            if datetime.date.today() == lastran:
+                vibe = await self.config.user(ctx.message.author).vibe()
+                await ctx.send("You rolled a **{}** today.".format(vibe))
             else:
-                comment = "Okay King"
+                await self.config.user(ctx.message.author).lastran.set(
+                    datetime.datetime.strftime(datetime.date.today(), "%Y-%m-%d")
+                )
+                    
+                vibe = time.time_ns() % 21  # Generate random number between 0 and 20
+                await self.config.user(ctx.message.author).vibe.set(vibe)
 
-            await ctx.send(":game_die: {} checked their vibe and got **{}**\n{}".format(
-                ctx.message.author.mention,vibe,comment)
-            )
+                comment = self._get_vibe_comment(vibe)
+                await ctx.send(":game_die: {} checked their vibe and got **{}**\n{}".format(
+                    ctx.message.author.mention, vibe, comment
+                ))
+        except Exception as e:
+            await ctx.send("An error occurred while checking your vibes. Please try again later.")
+            print(f"Error in vibecheck: {e}")
