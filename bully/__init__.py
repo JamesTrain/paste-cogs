ea8f37f90363a018f5b764bb8657ccebf3451cc6
James Gallagher <3924648+JamesTrain@users.noreply.github.com>
2025-06-25 19:08:54 -0400

    Fixing Dan's stupid mistake

--- bully/__init__.py
+++ bully/__init__.py
@@ -1,7 +1,7 @@
-from .bully import bully
+from .bully import Bully
 
 
 async def setup(bot):
-    await bot.add_cog(bully(bot))
+    await bot.add_cog(Bully(bot))
 
 # Author - Daniel Bush, A.K.A. Daddy
